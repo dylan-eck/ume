4d5d6b539520c475b3716746543f95f9efe5b33a
Dylan Eck <akdylaneck@gmail.com>
2026-07-31 13:47:32 -0400

    make render backend selectable at build

--- build.sh
+++ build.sh
@@ -4,10 +4,7 @@ set -euo pipefail
 preset="$1"
 build_dir="build/$preset"
 
-if [[ ! -f "$build_dir/CMakeCache.txt" ]]; then
-    cmake --preset "$preset"
-fi
-
+cmake --preset "$preset"
 cmake --build --preset "$preset"
 
 ln -sf "$build_dir/compile_commands.json" compile_commands.json
