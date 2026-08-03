70e88668464e64d0a283cb9fe167a85b13174004
Dylan Eck <akdylaneck@gmail.com>
2026-08-02 23:55:04 -0400

    remove index count from draw command

--- src/ume/renderer/renderer.cpp
+++ src/ume/renderer/renderer.cpp
@@ -84,7 +84,6 @@ void Renderer::render() {
         backend_->draw({
             .vertex_buffer = mesh.vertex_buffer,
             .index_buffer = mesh.index_buffer,
-            .index_count = mesh.index_count,
             .index_type = mesh.index_type,
             .push_constants = std::as_bytes(std::span(&uniforms, 1)),
         });
